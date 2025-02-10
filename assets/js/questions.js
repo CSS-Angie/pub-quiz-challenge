99759b33695b1a3e5645cfc16952697aa07a2a1e
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-10 16:27:53 +0100

    add function select answer and first list of questions - easy

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,10 +1,5 @@
 
 
-function showName() {
-    document.getElementById("name");.addEventListener("keydown", function(event) {
-        if (event.key ==="Enter") {
-            console.log(`Hello ${playerName}!`);
-        }
-    }
-    
-}
+
+
+
