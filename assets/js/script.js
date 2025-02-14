190f389e4b05fd88175d4a7be8fb797ae2a89e0f
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-14 15:22:06 +0100

    add images to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,191 +3,223 @@
 let questionsEasy = [ 
     {
         question: "Who was the first Disney character created by Walt Disney?",
+        imgSrc: "../images/quiz-images/mickey-and-friends.webp",
         answers: ["Alladin ", "Donald Duck", "Mickey Mouse",],
         correct: "Mickey Mouse",
     },
     {
         question: "Which painting is also known as La Joconde?",
+        imgSrc: "../images/quiz-images/painting-nciofranzart.webp",
         answers: ["Girl with the pearl earring", "Mona Lisa", "Woman with a mirror",],
         correct: "Mona Lisa",
     },
     {
         question: "Who sings Poker Face?",
+        imgSrc: "../images/quiz-images/pokerface-roger.webp",
         answers: ["Lady Gaga", "Madonna ", "Taylor Swift",],
         correct: "Lady Gaga",
     },
     {
         question: "What are the names of Cinderella’s evil stepsisters?",
+        imgSrc: "../images/quiz-images/cinderella-substackcdn.webp",
         answers: ["Anastasia and Drizella", "Gizelle and Anabelle", "Florence and Marge",],
         correct: "Anastasia and Drizella",
     },
     {
         question: "Which College Is Elle Applying for In Legally Blonde?",
+        imgSrc: "../images/quiz-images/legally-blonde-stylecaster.webp",
         answers: ["Yale", "Harvard", "Princeton",],
         correct: "Harvard",
     },
     {
         question: "What’s the name of the sword in The Sword In The Stone?",
+        imgSrc: "../images/quiz-images/swordinthestone-gathertales.webp",
         answers: ["Glamdring", "Callandor", "Excalibur",],
         correct:  "Excalibur",
     },
     {
         question: "Stark Industries is associated with which fictonal superhero?",
+        imgSrc: "../images/quiz-images/starkindustries-pxfuel.webp",
         answers: ["Iron Man", "Iron Fist", "Captain America",],
         correct: "Iron Man",
     },
     {
         question: "Which Museum Is Featured In Night at the Museum?",
+        imgSrc: "../images/quiz-images/night-museum-medium.webp",
         answers: ["Museum of Natural History", "The Smithsonian", "National Museum of the American Indian",],
         correct: "Museum of Natural History",
     },
     {
         question: "The Playstation game console was developed by which company?",
+        imgSrc: "../images/quiz-images/PS5-playstation.webp",
         answers: ["Nintendo", "Sega", "Sony",],
         correct: "Sony",
     },
     {
         question: "Which part of his body did Charlie Chaplin insure?",
+        imgSrc: "../images/quiz-images/charliechaplin-borsonline.hu.webp",
         answers: ["Moustache ", "Hands ", "Feet ",],
         correct: "Feet ",
     },
     {
         question: "What is the name of the element with the chemical symbol ‘He’?",
+        imgSrc: "../images/quiz-images/he-cleanpng.webp",
         answers: ["Helium", "Hydrogen", "Holmium",],
         correct: "Helium",
     },
     {
         question: "Which animal can be seen on the Porsche logo?",
+        imgSrc: "../images/quiz-images/porsche-porsche.webp",
         answers: ["Cougar", "Horse", "Cheetah",],
         correct: "Horse",
     },
     {
         question: "What type of scientist studies living plants?",
+        imgSrc: "../images/quiz-images/plant-teenagertoday.webp",
         answers: ["Paleontologist", "Entomologist", "Botanist",],
         correct: "Botanist",
     },
     {
         question: "What do you call traditional Japanese female entertainers who act as hostesses and whose skills include performing various Japanese arts?",
+        imgSrc: "../images/quiz-images/japaneselady-mitsubishi.webp",
         answers: ["Hakama", "Geisha", "Maiko ",],
         correct: "Geisha",
     },
     {
         question: "The Aztecs even used cocoa beans as what?",
+        imgSrc: "../images/quiz-images/cocoa-beans-indamart.webp",
         answers: ["Currency", "Drugs", "Weapon decorations",],
         correct: "Currency",
     },
     {
-        question: "'Mama Mia' is based on a song by which Swedish musical act?",
-        answers: ["ABBA", "Bee Gees", "Queen",],
-        correct: "ABBA",
-    },
-    {
         question: "	In Norse mythology, what is Thor the God of?",
+        imgSrc: "../images/quiz-images/thor-comicbookclublive.webp",
         answers: ["Rain", "Dancing", "Thunder",],
         correct: "Thunder",
     },
     {
         question: "Where is the Great Wall Located?",
+        imgSrc: "../images/quiz-images/thegratwall-bbc.webp",
         answers: ["China", "South Korea", "North Korea",],
         correct: "China",
     },
     {
         question: "Mr. Pibb was a soft drink created by the Coca-Cola Company to compete with what other soft drink?",
+        imgSrc: "../images/quiz-images/mrpibb-picclickimg.webp",
         answers: ["Cherry Cola", "Dr. Pepper", "Root beer",],
         correct: "Dr. Pepper",
     },
     {
         question: "Who was married to John F. Kenedy and was first lady from 1961 until 1963?",
+        imgSrc: "../images/quiz-images/jkf-with-wife-fineartstorehouse.webp",
         answers: ["Michelle LaVaughn Robinson Kenedy", "Eleanor Kenedy", "Jacqueline Kennedy Onassis",],
         correct: "Jacqueline Kennedy Onassis",
     },
     {
         question: "What year did the Chernobyl disaster occur?",
+        imgSrc: "../images/quiz-images/chernobyl-destinationsunknown.webp",
         answers: ["1986", "1984", "1985",],
         correct: "1986",
     },
     {
         question: "A Blue Whale has a heart roughly the size of a what?",
+        imgSrc: "../images/quiz-images/blue-whale-danawharf.webp",
         answers: ["VW Beetle", "Basketball", "Grapefruit",],
         correct: "VW Beetle",
     },
     {
         question: "Which country flag, nicknamed “The Maple Leaf’ consists of a red field with a white square and features a red maple leaf at its center?",
+        imgSrc: "../images/quiz-images/maple-leaf-flag-lestresorsderable.webp",
         answers: ["Turkey", "Canada", "Colombia",],
         correct: "Canada",
     },
     {
         question: "How would Groot answer this question?",
+        imgSrc: "../images/quiz-images/groot-goodfon.webp",
         answers: ["YO homie whats up!", "I am groot", "Groot I am young padawan",],
         correct: "I am groot",
     },
     {
         question: "What did Aladdin steal in the marketplace at the beginning of “Aladdin”?",
+        imgSrc: "../images/quiz-images/aladdin-disneyplus.webp",
         answers: ["Apple", "Gold", "Bread",],
         correct: "Bread",
     },
     {
         question: "What popular beverage once contained cocaine?",
+        imgSrc: "../images/quiz-images/cocaine-springhillrecoverycenter.webp",
         answers: ["Coca-Cola", "Schweppes", "Dr Pepper",],
         correct: "Coca-Cola",
     },
     {
         question: "Henry John Heinz founded a company specializing in the production of which food product?",
+        imgSrc: "../images/quiz-images/heinz-archbridgeinstitute.org.webp",
         answers: ["Mustard", "Ketchup", "Relish",],
         correct: "Ketchup",
     },
     {
         question: "What is the primary ingredient in guacamole?",
+        imgSrc: "../images/quiz-images/guacamole.webp",
         answers: ["Tomato", "Pineapple", "Avocado",],
         correct: "Avocado",
     },
     {
         question: "Which animal is, according to the New York times, by far the most expensive animal to keep in a zoo?",
+        imgSrc: "../images/quiz-images/zoo.rehlat.ae.webp",
         answers: ["Hippo", "Elephant", "Giant panda",],
         correct: "Giant panda",
     },
     {
         question: "Which is an Icelandic traditional dish?",
+        imgSrc: "../images/quiz-images/icelandiv-tourhq.webp",
         answers: ["Sheep’s head", "Rugbrød", "Lutefisk",],
         correct: "Sheep’s head",
     },
     {
         question: "Which country does gouda cheese come from?",
+        imgSrc: "../images/quiz-images/gouda-cheese-kazecheese.webp",
         answers: ["Belgium", "Switzerland", "Netherlands",],
         correct: "Netherlands",
     },
     {
         question: "Worldwide, what is the third most popular drink?",
+        imgSrc: "../images/quiz-images/popular-drinks-calow-drinks.webp",
         answers: ["Beer", "Water", "Coffee",],
         correct: "Beer",
     },
     {
         question: "What is Mickey Mouses dog called?",
+        imgSrc: "../assets/images/quiz-images/mickey-and-dog.webp",
         answers: ["Bruce", "Jude", "Pluto",],
         correct: "Pluto", 
     },
     {
         question: "What are Santa’s little helpers called?",
+        imgSrc: "../images/quiz-images/elf.webp",
         answers: ["Gremlins", "Elves", "Little People",],
         correct: "Elves",
     },
     {
         question: "In the following which one food Giant Pandas normally eat?",
+        imgSrc: "../images/quiz-images/panda-static-dw.webp",
         answers: ["Bamboo", "Corn", "Fish",],
         correct: "Bamboo",
     },
     {
         question: "In the movie Finding Nemo, which country has Nemo been taken to?",
+        imgSrc: "../images/quiz-images/nemo-charactour.webp",
         answers: ["England", "Australia", "New Zealand",],
         correct: "Australia",
     },
     {
         question: "What type of animals pull Santa’s sleigh?",
+        imgSrc: "../images/quiz-images/sleigh-santa.webp",
         answers: ["Dogs", "Horses", "Reindeer",],
         correct: "Reindeer",
     },
     {
         question: "What color is the Grinch, who stole Christmas?",
+        imgSrc: "../images/quiz-images/grinch-gamebuster.webp",
         answers: ["Green", "Blue", "Brown",],
         correct: "Green",
     }
@@ -207,7 +239,6 @@ const scoreCorrect = 100;
 const maxQuestions = 10;
 /**const answers = Array.from(document.getElementsByClassName("button-answer"));*/
 
-let currentQuestion = {};
 let acceptingAnswers = true;
 let score = 0;
 let questionCount = 0;
@@ -224,19 +255,29 @@ runGame = () => {
 }
 
 /**New question is generated and displayed from the array */
+
 NewQuestion = () => {
+
+/** display question */
 const questionIndex = Math.floor(Math.random()* questionsEasy.length);
 
-currentQuestion =  availableQuestions[questionIndex];
+const currentQuestion =  availableQuestions[questionIndex];
 question.innerText = currentQuestion.question;
 
-const AnswerA = document.getElementById("AnswerA");
-const AnswerB = document.getElementById("AnswerB");
-const AnswerC = document.getElementById("AnswerC");
+/** display image */
+function displayImage () {
+    document.getElementById("image-area").queryselector("img").src = currentQuestion.imgSrc;
+}
 
-AnswerA.innerHTML=questionsEasy[questionIndex].answers[0];
-AnswerB.innerHTML=questionsEasy[questionIndex].answers[1];
-AnswerC.innerHTML=questionsEasy[questionIndex].answers[2];
+/** display answer */
+const answerA = document.getElementById("answerA");
+const answerB = document.getElementById("answerB");
+const answerC = document.getElementById("answerC");
+
+answerA.innerHTML=currentQuestion.answers[0];
+answerB.innerHTML=currentQuestion.answers[1];
+answerC.innerHTML=currentQuestion.answers[2];
+
 console.log(currentQuestion);
 acceptingAnswers = true;
 checkAnswer();
@@ -247,43 +288,40 @@ availableQuestions.splice(questionIndex, 1);
 }
 
 /**
- * checking the correctness of answer after clicking it
+ * checking the correctness of answer after clicking it 
  */
-answerButtons.forEach(answerButton) {
-        answerButton.addEventListener("click", e => {
-            checkAnswer(anwserButton.textContent);
+function checkAnswer(selectedAnswer) {
+    let correctAnswer = questionsEasy[questionIndex].correct;
 
-            questionCount + 1;
+    answerButtons.forEach(answerButton => {
+        answerButton.addEventListener("click", e => {
+            checkAnswer(answerButton.textContent);
+
+            questionCount += 1;
         
-    if (questionIndex <= maxQuestions) {
-        return NewQuestion ();
+    if (questionIndex < maxQuestions) {
+        return NewQuestion();
     } else {
         showScore();
     }}
 )
 });
 
-
-
-/**
- * counts up when answer is correct
- */
-function checkAnswer(selectedAnswer) {
-    let correctAnswer = questionsEasy[questionIndex].correct;
-
     if (selectedAnswer === correctAnswer) {
         incrementScore(); 
-        selectedAnswer.classList.add("correct");
+        answerButton.classList.add("correct");
     } else {
         score (0);
-        selectedAnswer.classList.add("incorrect")
+        answerButton.classList.add("incorrect")
 
     };
 }
 
+/** counts up when answer is correct */
+
 function incrementScore () {
         let finalScore = parseInt(document.getElementById("final-score").innerText);
-        document.getElementById("final-score").textContent = oldScore + 100;
+        document.getElementById("final-score").textContent = finalScoreScore + 100;
 };
 
 /**
@@ -335,7 +373,7 @@ document.addEventListener("DOMContentLoaded", function() {
         
             /**
             displayEasyQuestions() {
-            return .textContent = questionsEasy.question;
+            return questionIndex.textContent = questionsEasy.question;
             }
             displayAverageQuestions() {
             return ..textContent = questionsAverage.question;
