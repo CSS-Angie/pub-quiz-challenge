7a9015a7e0fbecba8d2983ac5e9e55f03e14fb1e
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-21 21:48:36 +0100

    change last page to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -331,7 +331,7 @@ function selectAnswer() {
         questionCount = questionCount++;
         console.log("question count")
         if (availableQuestions.length = 0 || questionCount > MAX_QUESTIONS) {
-            showScore();
+            showModal();
             console.log("show score")
         } else {
             newQuestion();
@@ -355,13 +355,39 @@ function incrementScore() {
 
 // show score at the the end
 function showScore() {
-    localStorage.setItem("mostRecentScore", score);
-    return window.location.assign("/result.html");
+   modal
     console.log(mostRecentScore);
 }
 
 function mostRecentScore() {
-
 }
+
+
+//function showModal() {
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+//}
 
 runGame();
