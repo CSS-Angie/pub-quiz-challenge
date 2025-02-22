2b54e4a306070bb861afeb8c20b40782d91d713a
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-22 19:45:55 +0100

    retry working startchallenge on indexpage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 // Displays Name of Player on first page after having submitted a name
 const confirmName = document.getElementById("confirm");
 const inputName = document.querySelector("input");
-const displayName = document.getElementById("display-name");
+let displayName = document.getElementById("display-name");
 
 console.log("const")
 confirmName.addEventListener("click", () => {
@@ -15,16 +15,15 @@ displayName.style.color = "#151c4a";
 
 // Open game after confirming name
 function startChallenge() {
+   
 document.getElementById("btn-start").onclick = function() {
-    if (displayName === undefined) {
+    let displayName = document.getElementById("display-name");
+    if (displayName.innerHTML === "") {
         alert(`Please confirm your name`);
 console.log("alert")
-    } else if (displayName !==""){
+    } else {
         console.log("html")
         window.location.href = "https://css-angie.github.io/pub-quiz-challenge/the-pub-quiz-challenge.html";
-    } else {
-     alert(`Please retry`);  
-     console.log("retry")
     }
 }
 }
