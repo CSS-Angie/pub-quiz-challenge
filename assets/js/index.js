7050938f37913f4bcd4684fe3f17303f7bcaa9f9
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-22 17:27:29 +0100

    add working startchallenge on indexpage

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,10 @@ const confirmName = document.getElementById("confirm");
 const inputName = document.querySelector("input");
 const displayName = document.getElementById("display-name");
 
+console.log("const")
 confirmName.addEventListener("click", () => {
-const inputValue = inputName.value;
+    console.log("click")
+    const inputValue = inputName.value;
 displayName.innerHTML = `Hello ${inputValue}!`;
 displayName.style.border = "1px solid #0069b0";
 displayName.style.backgroundColor = "#f88839";
@@ -13,25 +15,31 @@ displayName.style.color = "#151c4a";
 
 // Open game after confirming name
 function startChallenge() {
-    let startButton = document.getElementById("btn-start")
-if (displayName.innerHTML.value !=="") {
+document.getElementById("btn-start").onclick = function() {
+    if (displayName === undefined) {
+        alert(`Please confirm your name`);
+console.log("alert")
+    } else if (displayName !==""){
+        console.log("html")
+        window.location.href = "https://css-angie.github.io/pub-quiz-challenge/the-pub-quiz-challenge.html";
+    } else {
+     alert(`Please retry`);  
+     console.log("retry")
+    }
+}
+}
+
+/**function startChallenge() {
+    document.getElementById("btn-start").addEventListener("click", startChallenge);
+    let displayName = document.getElementById("btn-start");
+if (displayName.innerHTML.value !=="") { 
     alert(`Please confirm your name`);
     console.log("alert")
 } else {
+    console.log("html")
     redirect();
-    function redirect() {
-        window.location.href="https://css-angie.github.io/pub-quiz-challenge/the-pub-quiz-challenge.html";
-        };
-   console.log("html")
 };
 }
-
-
+*/
 
 startChallenge();
-//Name in local storage for last page
-/**localStorage.setItem("input", "inputValue");
-document.getElementById("local-name").innerHTML = localStorage.getItem("input");
-
-`<p ><span id="local-name">Name,<br>here's your final score!</span></p>`
-*/
