944d5bea6e6fd5ad9843c7379b095b95b7461967
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-22 20:03:22 +0100

    js for button on 404

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,30 +3,30 @@ const confirmName = document.getElementById("confirm");
 const inputName = document.querySelector("input");
 let displayName = document.getElementById("display-name");
 
-console.log("const")
 confirmName.addEventListener("click", () => {
-    console.log("click")
     const inputValue = inputName.value;
-displayName.innerHTML = `Hello ${inputValue}!`;
-displayName.style.border = "1px solid #0069b0";
-displayName.style.backgroundColor = "#f88839";
-displayName.style.color = "#151c4a";
+    displayName.innerHTML = `Hello ${inputValue}!`;
+    displayName.style.border = "1px solid #0069b0";
+    displayName.style.backgroundColor = "#f88839";
+    displayName.style.color = "#151c4a";
 });
 
 // Open game after confirming name
 function startChallenge() {
-   
-document.getElementById("btn-start").onclick = function() {
-    let displayName = document.getElementById("display-name");
-    if (displayName.innerHTML === "") {
-        alert(`Please confirm your name`);
-console.log("alert")
-    } else {
-        console.log("html")
-        window.location.href = "https://css-angie.github.io/pub-quiz-challenge/the-pub-quiz-challenge.html";
-    }
-}
+    document.getElementById("btn-start").onclick = function () {
+        let displayName = document.getElementById("display-name");
+        if (displayName.innerHTML === "") {
+            alert(`Please confirm your name`);
+        } else {
+            window.location.href =
+                "https://css-angie.github.io/pub-quiz-challenge/the-pub-quiz-challenge.html";
+        }
+    };
 }
 startChallenge();
 
 
+document.getElementById("btn-return").onclick = function () {
+    window.location.href =
+        "index.html";
+}
