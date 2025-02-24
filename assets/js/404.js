240387da0c50a16c7f28b187bde8d43c2257883c
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-24 14:34:51 +0100

    second retry fix 404 button to homepage

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,4 +1,8 @@
-document.getElementById("btn-return").onclick = function () => {
+function returnToHomepage(){
+document.getElementById("btn-return").onclick = function () {
     window.location.href =
         "https://css-angie.github.io/pub-quiz-challenge/";
 };
+}
+
+returnToHomepage();
