0f66a28a65203a5d5326708d369476f12655e2e5
CSS-Angie <gaf7y96r@students.codeinstitute.net>
2025-02-24 14:48:02 +0100

    delete retry 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,8 +1,8 @@
-function returnToHomepage(){
+
 document.getElementById("button-return").onclick = function () {
     window.location.href =
         "https://css-angie.github.io/pub-quiz-challenge/";
 };
-}
 
-returnToHomepage();
+
+
